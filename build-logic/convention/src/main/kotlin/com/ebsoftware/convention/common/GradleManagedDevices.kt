0b81651af63f50d25f70b3ce4939edfa3e240911
Endre Borcsok <endre.borcsok@starlingbank.com>
2024-05-04 15:15:06 +0100

    test device tweak

--- build-logic/convention/src/main/kotlin/com/ebsoftware/convention/common/GradleManagedDevices.kt
+++ build-logic/convention/src/main/kotlin/com/ebsoftware/convention/common/GradleManagedDevices.kt
@@ -8,7 +8,7 @@ import org.gradle.kotlin.dsl.invoke
 internal fun configureGradleManagedDevices(
     commonExtension: CommonExtension<*, *, *, *, *, *>,
 ) {
-    val pixel6 = DeviceConfig("Pixel 4", 30, "aosp-atd")
+    val pixel6 = DeviceConfig("Pixel 4", 33, "aosp-atd")
 
     val allDevices = listOf(pixel6)
     val ciDevices = listOf(pixel6)
